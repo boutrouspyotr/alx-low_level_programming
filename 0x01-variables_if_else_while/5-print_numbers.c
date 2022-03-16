3b51ec284d0590698505a6e5fb11fd9445cc2f0d
boutrouspyotr <boutrouspyotr1@gmail.com>
2022-03-16 08:58:30 +0300

    Coding C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	while (a < 10)
+	{
+		printf("%d\n", a);
+		a++;
+	}
+	return (0);
+}
