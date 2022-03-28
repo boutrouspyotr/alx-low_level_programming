649f8d962768bc029c48a8ca6c4bc44082404efb
boutrouspyotr <boutrouspyotr1@gmail.com>
2022-03-28 12:45:58 +0300

    Coding C

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 x
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void);
+{
+	char h;
+
+	int v = 0;
+
+	while (v < 10)
+	{
+		h = 'a';
+		while (h <= 'z')
+		{
+			_putchar(h);
+			h++;
+		}
+		_putchar('\n');
+		v++;
+	}
+}
