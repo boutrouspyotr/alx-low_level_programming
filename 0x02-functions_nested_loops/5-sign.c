7d4a2e426c3595483c39c1a51196387ddfd38474
boutrouspyotr <boutrouspyotr1@gmail.com>
2022-03-29 17:05:44 +0300

    Coding C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of the integer
+ *
+ * @n : is the integer
+ *
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	int p;
+
+	if (n > 0)
+	{
+		p = 1;
+		_putchar('+');
+	}
+	elseif(n == 0)
+	{
+		p = 0;
+		_putchar('0');
+	}
+	else
+	{
+		p = -1;
+		_putchar("-");
+	}
+	return (p);
+}
