87c8e1d532632018d416ef789b91f4b37e460724
boutrouspyotr <boutrouspyotr1@gmail.com>
2022-06-03 22:46:31 +0300

    doing work

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ *
+ */
+int main(void)
+{
+	a = '0';
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
